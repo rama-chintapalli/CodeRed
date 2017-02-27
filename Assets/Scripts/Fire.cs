5e200d21df174d18180b05fc0513ef63aafd798e
rama-chintapalli <ramaswamych@gmail.com>
2017-02-27 00:40:34 -0800

    add scene, fire , extinguisher, UI

---
+++ Assets/Scripts/Fire.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+	public GameObject cube;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void onClick(){
+		cube.GetComponent<Renderer>().material.color = new Color(Random.Range(0.0f,1.0f),Random.Range(0.0f,1.0f),Random.Range(0.0f,1.0f));
+	}
+}
---
+++ Assets/Scripts/MenuPanelController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPanelController : MonoBehaviour {
+	public GameObject menuPanel;
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		menuPanel.SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (GvrController.AppButtonDown) {
+			// Do something.
+			// TouchDown is true for 1 frame after touchpad is touched.
+			Debug.Log("MenuPanelController : App Button Down" +menuPanel.activeSelf );
+			/*menuPanel.transform.position = player.transform.position; 
+			menuPanel.transform.rotation = player.transform.rotation; 
+			menuPanel.transform.Translate(0.0f, 0.0f, 0.6f); // move away from the camera 
+			menuPanel.transform.LookAt(player.transform.position);*/
+		
+
+			//menuPanel.SetActive (!menuPanel.activeSelf);
+
+		}
+	}
+}
